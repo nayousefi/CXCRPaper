348772be52a5072e8e2c4356544bfd6b5d5a7424
nyousefi <nikolas.yousefi.103@my.csun.edu>
2022-06-08 16:14:42 -0700

    Melanocyte + melanoma ANOVA

---
+++ Scripts/2022-06-08_Melanocyte_Melanoma_Wounds_ANOVA.R
@@ -0,0 +1,37 @@
+############# Melanocyte and Melanoma Wound Stats Analysis ########
+
+############# By Roland Lacap and Nikolas Yousefi #########
+
+############# Created on 2022-06-08 #########
+
+### Load libraries
+
+library(tidyverse)
+library(ggpubr)
+library(here)
+
+melanocyte_wound <- read.csv(here("Data", "2022-06-06_Melanocyte_WOUNDS_Master_Sheet.csv"))
+
+melanoma_wound <- read.csv(here("Data", "2022-06-02_Melanoma_WOUNDS_Master_Sheet.csv"))
+
+melanocyte_wound_pivot <- melanocyte_wound %>% 
+  pivot_longer(cols = c(6:9),
+               names_to = "Time_Period",
+               values_to = "Values")
+
+melanoma_wound_pivot <- melanoma_wound %>% 
+  pivot_longer(cols = c(6:9),
+               names_to = "Time_Period",
+               values_to = "Values")
+
+melanocyte_melanoma_wounds <- full_join(melanocyte_wound_pivot, melanoma_wound_pivot, copy = FALSE)
+
+summary(melanocyte_melanoma_wounds)
+
+melanocyte_melanoma_anova <- aov(Values ~ Cell_type + Time_Period, data = melanocyte_melanoma_wounds)
+
+melanocyte_melanoma_anova_sum <- summary(melanocyte_melanoma_anova)
+
+capture.output(melanocyte_melanoma_anova_sum, file = "melanocyte_melanoma_wound_anova_results_2022-06-08.doc")
+
+write.csv(here("Data", "melanocyte_melanoma_wounds.csv"))
